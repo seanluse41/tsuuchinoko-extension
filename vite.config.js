c428615dec3045148d6b41fd8690c40910911378
seanluse <sean@kintone.com>
2025-03-11 13:55:07 +0900

    update config and manifest maybe

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [svelte()],
   build: {
     outDir: 'dist',
     rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        background: resolve(__dirname, 'src/background.js'),
+        content: resolve(__dirname, 'src/content.js'),
+        passCybozuData: resolve(__dirname, 'src/pass-cybozu-data.js')
       }
     }
   }
